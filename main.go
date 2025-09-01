c02de21e34118a89fd916d88e791745f26ede743
Benoit.G <benoitgaugiran@gmail.com>
2025-09-01 23:10:32 +0200

    Communications par channels

--- main.go
+++ main.go
@@ -15,13 +15,33 @@ func task(id int, wg *sync.WaitGroup) {
 	fmt.Printf("Task %v done\n", id)
 }
 
+func producer(value int, values chan int) {
+	for i := range value {
+		values <- i
+	}
+	close(values)
+}
+
+func consumer(worker int, values chan int) {
+	for value := range values {
+		fmt.Printf("worker %v processed %v \n", worker, value)
+	}
+}
+
 func main() {
 	wg := &sync.WaitGroup{}
-	for w := 0; w < 5; w++ {
-		wg.Add(1)
-		go task(w, wg)
+	values := make(chan int)
+	workers := 3
 
+	go producer(10, values)
+
+	for w := range workers {
+		wg.Add(1)
+		go func(worker int) {
+			defer wg.Done()
+			consumer(w, values)
+		}(w)
 	}
 	wg.Wait()
-	fmt.Println("All tasks completed")
+
 }
