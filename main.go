3c1fe0584b4a7925a7ccb67fc01188e44f3fa723
Benoit.G <benoitgaugiran@gmail.com>
2025-09-02 22:40:46 +0200

    Select concurrence

--- main.go
+++ main.go
@@ -2,78 +2,43 @@ package main
 
 import (
 	"fmt"
-	"math/rand/v2"
-	"sync"
 	"time"
 )
 
-func task(id int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	randomTime := time.Duration(rand.IntN(5)+1) * time.Second
-	fmt.Printf("Task %v started... It will take %v \n", id, randomTime)
-	time.Sleep(randomTime)
-	fmt.Printf("Task %v done\n", id)
-}
-
-func producer(value int, values chan int) {
-	for i := range value {
-		values <- i
-	}
-	close(values)
-}
-
-func fanin(ch1 chan int, ch2 chan int) chan int {
-	var wg sync.WaitGroup
-	wg.Add(2)
-	aggregatedChannel := make(chan int, cap(ch1)+cap(ch2))
-	go func() {
-		defer wg.Done()
-		for val := range ch1 {
-			aggregatedChannel <- val
-		}
-	}()
-	go func() {
-		defer wg.Done()
-		for val := range ch2 {
-			aggregatedChannel <- val
-		}
-
-	}()
-	go func() {
-		wg.Wait()
-		close(aggregatedChannel)
-
-	}()
-
-	return aggregatedChannel
-}
-
-func consumer(worker int, values chan int) {
-	for value := range values {
-		fmt.Printf("worker %v processed %v \n", worker, value)
-	}
-}
-
 func main() {
-	wg := &sync.WaitGroup{}
+	ch1 := make(chan string)
+	ch2 := make(chan string)
 
-	nbProcess := 100
-	ch1 := make(chan int, nbProcess)
-	ch2 := make(chan int, nbProcess)
-	workers := 3
+	go func() {
 
-	producer(nbProcess, ch1)
-	producer(nbProcess, ch2)
+		time.Sleep(1 * time.Second)
+		ch1 <- "Salut c'est fonction 1"
+		close(ch1)
+	}()
 
-	channel := fanin(ch1, ch2)
+	go func() {
 
-	for w := range workers {
-		wg.Add(1)
-		go func(worker int) {
-			defer wg.Done()
-			consumer(w, channel)
-		}(w)
+		time.Sleep(4 * time.Second)
+		ch2 <- "Moi c'est fonction 2 "
+		close(ch2)
+	}()
+
+	for ch1 != nil || ch2 != nil {
+		select {
+		case m1, ok := <-ch1:
+			if !ok {
+				ch1 = nil
+				continue
+			}
+			fmt.Printf("Message reçu de la part de fonction 1, voici son message : %s", m1)
+		case m2, ok := <-ch2:
+			if !ok {
+				ch2 = nil
+				continue
+			}
+			fmt.Printf("Message reçu de la part de fonction 2, voici son message : %s", m2)
+		}
+
 	}
-	wg.Wait()
-
+	fmt.Println("Select terminé")
 }
